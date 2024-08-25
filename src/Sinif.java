a8a2680738db1f8257cb28aeae0cc14f9c9886d8
MuhammetYazici <yzcmm61@gmail.com>
2024-08-25 15:18:43 +0300

    değişiklik yapıldı

--- src/Sinif.java
+++ src/Sinif.java
@@ -2,5 +2,6 @@ public class Sinif {
     public static void main(String[] args) {
         System.out.println("Merhaba Team 3");
         System.out.println("merhaba burası branchBetul");
+        System.out.println("değişiklik yapıldı");
     }
 }
